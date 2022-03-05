17a90030015440bccd9ee6d7acd909282f076551
Prince Ibewiro <pibewiro@yahoo.com>
2022-03-04 23:42:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,153 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
+
+const itemsFromBackend = [
+  { id: uuid(), content: "First Task" },
+  { id: uuid(), content: "Second Task" },
+];
+
+const columnsFromBackend = {
+  monday: {
+    name: "Mon",
+    items: [],
+  },
+  tuesday: {
+    name: "Tue",
+    items: [],
+  },
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
 
 function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+      <DragDropContext
+        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+      >
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <div style={{ marginRight: 60 }}>
+              <h2>{column.name}</h2>
+              <div>
+                <select
+                  onClick={(e) => {
+                    const data = JSON.parse(e.target.value);
+
+                    setColumns({
+                      ...columns,
+                      [id]: {
+                        ...column,
+                        items: [
+                          ...column.items,
+                          { id: data.id, content: data.content },
+                        ],
+                      },
+                    });
+
+                    itemsFromBackend.splice(0, 1);
+                  }}
+                >
+                  {itemsFromBackend.map((x, i) => (
+                    <option
+                      value={JSON.stringify({ id: x.id, content: x.content })}
+                    >
+                      {x.content}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <Droppable droppableId={id} key={id}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "lightgrey",
+                        padding: 4,
+                        width: 250,
+                        minHeight: 500,
+                      }}
+                    >
+                      {column.items.map((item, index) => (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              style={{
+                                userSelect: "none",
+                                padding: 16,
+                                margin: "0 0 8px 0",
+                                minHeight: 50,
+                                backgroundColor: snapshot.isDragging
+                                  ? "#263b4a"
+                                  : "#456c86",
+                                color: "#fff",
+                                ...provided.draggableProps.style,
+                              }}
+                            >
+                              {item.content}
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
